577c5a77af18debb2b0f4a1861ddf141e1a8df9f
Wiktor <wpiela9@gmail.com>
2023-03-24 07:37:13 +0100

    first configuration, user model extended

---
+++ accounts/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key = True)
+    
+    bio = models.TextField(max_length=500, blank=True)
+    
+    roles = [
+        ("DEV", "developer"),
+        ("STU", "dtudent"),
+        ("INT", "internship"),
+        ("MAN", "project manager"),
+        ("EMP", "employer"),
+        ("ADM", "admin"),
+        ("TEC", "teacher"),
+        ("OTH", "other"),
+    ]
+
+    role = models.CharField(
+        max_length=3,
+        choices=roles,
+        default="ADM"
+    )
+
+    avatar = models.ImageField(upload_to="images/", default="default_user_img.jpg")
+
+    stop_notifications = models.BooleanField(default=False,
+                                             help_text= "if user wants to turn off email notification when question has been answered")
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
+
